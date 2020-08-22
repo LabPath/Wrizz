8459c029584cd642f7f511086187a02c3756ef2e
Ely <thevortexspirit@outlook.com>
2020-08-22 19:54:47 -0400

    filtered text posts

--- src/structures/reddit/RedditClient.js
+++ src/structures/reddit/RedditClient.js
@@ -1,16 +1,13 @@
 import { Server } from 'ws'
-import { get } from 'superagent'
 import cs from 'clean-stack'
 import request from 'request'
 let date = Math.floor(Date.now() / 1000)
 
-const error = (event, err) => event.emit('error', new Error(err));
-
-const fetch = async sub => {
+const fetch = async () => {
     return new Promise(resolve => {
         try {
             request({
-                url: `https://reddit.com/r/${sub}/new.json?limit=1`,
+                url: `https://reddit.com/r/Lab_path/new.json?limit=1`,
                 json: true
             }, async (err, res, body) => {
                 if (!res) res = { statusCode: 404 };
@@ -21,6 +18,7 @@ const fetch = async sub => {
                         if (date <= post.data.created_utc) {
                             date = post.data.created_utc;
 
+                            if (post.data.thumbnail === 'self') return
                             posts.push(post.data)
                         }
                     };
@@ -40,47 +38,7 @@ const fetch = async sub => {
 export default class RedditClient {
     constructor() {
         this.events = new Server({ port: 1034 });
-        this.list = new Set()
     };
-
-    async add(sub) {
-        const res = await this.get(sub);
-
-        if (!res) return this;
-        if (!this.list.has(sub)) this.list.add(sub);
-
-        return this;
-    }
-
-    async remove(sub) {
-        const res = await this.get(sub);
-
-        if (!res) return this;
-        if (this.list.has(sub)) this.list.delete(sub);
-
-        return this;
-    }
-
-    async get(sub) {
-        const res = await this.isValid(sub);
-
-        if (res !== true) {
-            this.events.emit('error', new Error(`The subreddit '${sub}' is invalid`));
-            return null;
-        };
-        return true;
-    }
-
-    async isValid(sub) {
-        const res = await get(`https://reddit.com/r/${sub}/about.json`).catch(() => null);
-
-        if (!res) return false;
-        if (res.status !== 200) return false;
-        if (res.body.data.hasOwnProperty('children')) {
-            if (res.body.data.children.length === 0) return false;
-        }
-        return true;
-    }
 
     on(event, listener) {
         this.events.on(event, listener);
@@ -88,17 +46,11 @@ export default class RedditClient {
     };
 
     async start() {
-        if (this.list.size === 0) return error(this.events, 'No subreddits added');
-
         setInterval(async () => {
-            this.list.forEach(async sub => {
-                if (sub) {
-                    const data = await fetch(sub);
-                    if (data.length !== 0) {
-                        this.events.emit('post', data);
-                    }
-                };
-            });
+            const data = await fetch();
+            if (data.length !== 0) {
+                this.events.emit('post', data);
+            }
         }, 3000);
         this.events.emit('ready', null);
     };
--- src/structures/reddit/RedditWatcher.js
+++ src/structures/reddit/RedditWatcher.js
@@ -14,7 +14,6 @@ export default class RedditWatcher {
         const guild = this.client.guilds.cache.get(process.env.GUILD_ID)
         const channel = guild.channels.cache.get(process.env.CHANNEL_ID)
 
-        await this.reddit.add('Lab_path')
         this.reddit
             .on('ready', () => this.client.logger.info('Connected to Reddit', { type: COLORS._REDDIT(TYPE.REDDIT), event: COLORS.CONNECT(EVENT.CONNECT)}))
             .on('post', data => channel.send('<@&725477327215132693>', new RedditEmbed({ post: data[0], title: 'Arcane Labyrinth' })))
