5dc957c6335229aaced27e030c19138b71ee3c68
Ely <thevortexspirit@outlook.com>
2020-08-27 22:26:28 -0400

    feat: member listeners

---
+++ src/listeners/guildMemberAdd.js
@@ -0,0 +1,26 @@
+import { Listener } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import { COLORS } from '../utils/constants'
+import moment from 'moment'
+
+export default class GuildMemberAdd extends Listener {
+	constructor() {
+		super('guildMemberAdd', {
+			emitter: 'client',
+			event: 'guildMemberAdd',
+		});
+	}
+
+	async exec(member) {
+        const memberlogs = this.client.settings.get(member.guild, 'memberlogs');
+
+		if (memberlogs) {
+			const embed = new MessageEmbed()
+            .setAuthor(`${member.user.tag} | ${member.id}`, member.user.displayAvatarURL())
+            .setFooter(`${member.guild.memberCount} Members • ${moment().format('MMMM DD, YYYY - hh:mm:ss A')}`)
+            .setColor(COLORS.JOIN)
+
+			return this.client.channels.cache.get(memberlogs).send(embed);
+		}
+	}
+}
---
+++ src/listeners/guildMemberRemove.js
@@ -0,0 +1,26 @@
+import { Listener } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import { COLORS } from '../utils/constants'
+import moment from 'moment'
+
+export default class GuildMemberRemove extends Listener {
+	constructor() {
+		super('guildMemberRemove', {
+			emitter: 'client',
+			event: 'guildMemberRemove',
+		});
+	}
+
+	async exec(member) {
+        const memberlogs = this.client.settings.get(member.guild, 'memberlogs');
+
+		if (memberlogs) {
+			const embed = new MessageEmbed()
+            .setAuthor(`${member.user.tag} | ${member.id}`, member.user.displayAvatarURL())
+            .setFooter(`${member.guild.memberCount} Members • ${moment().format('MMMM DD, YYYY - hh:mm:ss A')}`)
+            .setColor(COLORS.LEAVE)
+
+			return this.client.channels.cache.get(memberlogs).send(embed);
+		}
+	}
+}
