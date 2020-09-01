1334fdb4ad6dac412ba0e5e5af73e3b38e6d12b7
Ely <thevortexspirit@outlook.com>
2020-08-31 20:55:28 -0400

    fix: date now displays properly

--- src/structures/reddit/RedditEmbed.js
+++ src/structures/reddit/RedditEmbed.js
@@ -3,27 +3,16 @@ import moment from 'moment'
 import { COLORS } from '../../utils/constants'
 
 export default class RedditEmbed extends MessageEmbed {
-    constructor({
-        post,
-        date = post.created_utc * 1000,
-        format = 'MMMM DD, YYYY',
-        title = post.title,
-        url = post.url,
-        footer = true,
-        timestamp = true,
-        data = {}
-    })
+    constructor(post) {
+        super()
+        
+        this.post = post
+        this.url = this.post.url
 
-    {
-        super(data)
-
-        this.setTitle(`${title}  |  ${moment(date).add(1, 'd').format(format)}`)
-        this.setURL(url)
-        this.setImage(url)
-        this.setTimestamp(timestamp)
+        this.setTitle(`${moment().add(1, 'd').format('MMMM DD, YYYY')}`)
+        this.setURL(this.url)
+        this.setImage(this.url)
+        this.setFooter(`Posted by u/${this.post.author} | ${moment().format('h:mm:ss A')}`)
         this.setColor(COLORS.REDDIT)
-
-        if (timestamp === true) this.setTimestamp(new Date(date))
-        if (footer === true) this.setFooter(`Posted by u/${post.author}`)
     }
 }
--- src/structures/reddit/RedditWatcher.js
+++ src/structures/reddit/RedditWatcher.js
@@ -1,7 +1,7 @@
 import RedditClient from './RedditClient'
 import RedditEmbed from './RedditEmbed'
-import { COLORS } from '../../utils/constants'
 import { TYPE, EVENT } from '../../utils/logger';
+import { COLORS } from '../../utils/constants'
 import cs from 'clean-stack'
 
 export default class RedditWatcher {
@@ -14,10 +14,16 @@ export default class RedditWatcher {
         const guild = this.client.guilds.cache.get(process.env.GUILD_ID)
         const channel = guild.channels.cache.get(process.env.CHANNEL_ID)
 
-        this.reddit
-            .on('ready', () => this.client.logger.info('Connected to Reddit', { type: COLORS._REDDIT(TYPE.REDDIT), event: COLORS.CONNECT(EVENT.CONNECT)}))
-            .on('post', data => channel.send('<@&725477327215132693>', new RedditEmbed({ post: data[0], title: 'Arcane Labyrinth' })))
-            .on('error', err => console.log(cs(err.stack)))
+        this.reddit.on('ready', () => {
+                this.client.logger.info('Connected to Reddit', { 
+                    type: COLORS._REDDIT(TYPE.REDDIT), 
+                    event: COLORS.CONNECT(EVENT.CONNECT)
+                })
+            })
+        .on('post', data => {
+            channel.send('<@&725477327215132693>', new RedditEmbed(data[0]))
+        })
+        .on('error', err => console.log(cs(err.stack)))
 
         await this.reddit.start()
     }
