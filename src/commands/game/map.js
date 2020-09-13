99cb405724dfe96cc45db91045e057bbb07ed894
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2020-09-13 18:43:14 -0400

    fix: adjust file name check

--- src/commands/game/map.js
+++ src/commands/game/map.js
@@ -1,7 +1,7 @@
 import { get } from 'superagent'
 import { Command } from 'discord-akairo'
 import { MessageEmbed } from 'discord.js'
-import { COLORS, MESSAGES } from '../../utils/constants'
+import { CLRS, MESSAGES } from '../../utils/constants'
 import moment from 'moment'
 import glob from 'glob'
 import RedditEmbed from '../../structures/reddit/RedditEmbed'
@@ -13,7 +13,7 @@ export default class Map extends Command {
             description: {
                 content: MESSAGES.COMMANDS.GAME.MAP.DESCRIPTION,
                 usage: '[--date=YYYY-MM-DD]',
-                examples: ['--date=2020-01-25', '--date=Feb 11', '--date=Apr 06 2020']
+                examples: ['--date=2020-01-27', '-d=2019-9-5', '--date=4-12-20']
             },
             category: 'game',
             args: [
@@ -21,7 +21,7 @@ export default class Map extends Command {
                     id: 'date',
                     type: 'string',
                     match: 'option',
-                    flag: '--date=',
+                    flag: ['--date=', '-d=']
                 },
             ]
         })
@@ -31,30 +31,29 @@ export default class Map extends Command {
         if (!date) {
             try {
                 const { body } = await get('https://www.reddit.com/r/Lab_path/new.json?sort=new').query({ limit: 1 });
-                return message.util.send(new RedditEmbed(body.data.children[0].data));
+                return message.util.send(new RedditEmbed(body.data.children[0].data, true));
             } catch (err) {
-                return message.util.send(MESSAGES.COMMANDS.GAME.MAP.ERR_FETCH)
+                return message.util.reply(MESSAGES.COMMANDS.GAME.MAP.ERR_FETCH)
             }
         }
+        
+        date = new Date(date).toISOString()
 
-        if (moment(date).isValid()) {
-            date = moment(date).format('YYYY-MM-DD')
-            glob(`./LabPath Maps/All/*${date}*.png`, (err, file) => {
-                if (file.length) {
-                    const img = file.toString().split('/').pop().replace(/<|>|#/g, '')
+        glob(`./Lab Path/Maps/All/*${date.split('T')[0]}*.png`, (err, file) => {
+            if (file.length) {
+                file.length > 1 ? file = file[0] : file
 
-                    const mapEmbed = new MessageEmbed()
-                    .attachFiles(file)
-                    .setTitle(`Arcane Labyrinth  |  ${moment(date).format('MMMM DD, YYYY')}`)
-                    .setImage(`attachment://${img}`)
-                    .setColor(COLORS.REDDIT)
+                const img = file.toString().split('/').pop().replace(/<|>|#/g, '')
 
-                    return message.util.send(mapEmbed)
-                }
-                return message.util.reply(MESSAGES.COMMANDS.GAME.MAP.ERR_NO_MAP(date))
-            })
-        } else {
-            return message.util.send(MESSAGES.COMMANDS.GAME.MAP.ERR_DATE(date))
-        }
+                const mapEmbed = new MessageEmbed()
+                .attachFiles(file)
+                .setTitle(`${moment(date).format('MMMM DD, YYYY')}`)
+                .setImage(`attachment://${img}`)
+                .setColor(CLRS.REDDIT)
+
+                return message.util.send(mapEmbed)
+            }
+            return message.util.reply(MESSAGES.COMMANDS.GAME.MAP.ERR_NO_MAP(date.split('T')[0]))
+        })
     }
 }
