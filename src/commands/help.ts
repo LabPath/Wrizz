52d0babd46e25378fca48d4fe002aa1613c4574f
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2020-10-02 22:23:14 -0400

    chore(commands): reflect command info changes

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,44 +8,52 @@ export default class Help extends Command {
             aliases: ['help'],
             description: {
                 content: cmd.help.description,
-                usage: '[command|alias]'
-            }
+                usage: '[command]',
+                examples: ['furniture']
+            },
+            args: [
+                {
+                    id: 'cmd',
+                    type: 'commandAlias'
+                }
+            ]
         });
     }
 
     async exec(message: Message, { cmd }) {
-        const prefix = (this.handler.prefix as PrefixSupplier)(message);
+        const prefix = await (this.handler.prefix as PrefixSupplier)(message);
         const embed = new MessageEmbed().setColor('249EA0');
 
         if (!cmd) {
             embed.setAuthor(`Command List  |  Prefix: ${prefix}`, this.client.user.displayAvatarURL());
 
             for (const category of this.handler.categories.values()) {
-                embed.addField(
-                    `❯ ${category.id}`,
-                    category
-                        .filter((c) => c.aliases.length > 0)
-                        .map((c) => `\`${c.aliases[0]}\``)
-                        .join(' ')
-                );
+                embed.setDescription(
+                    category.filter((cmd) => cmd.aliases.length > 0)
+                    .map((cmd) => `\`${cmd.aliases[0]}\``).join(' ')
+                )
             }
+
             return message.util?.send(embed);
         }
 
-        const { aliases, description, category } = cmd;
+        const { aliases, description } = cmd;
 
         embed
-            .setAuthor(`${category.id}  |  ${prefix + aliases[0]}`)
+            .setAuthor(prefix + aliases[0])
             .setDescription(description.content)
-            .addField('❯ Aliases', aliases.length > 1 ? `\`${aliases.slice(1).join('` `')}\`` : 'No Aliases');
 
-        if (description.usage) embed.addField('❯ Usage', `\`${prefix + aliases[0]} ${description.usage}\``);
-
-        if (description.examples)
-            embed.addField(
-                '❯ Examples',
-                `${description.examples.map((ex) => `\`${prefix + aliases[0]} ${ex}\`\n`).join(' ')}`
-            );
+        if (aliases.length > 1) {
+            embed.addField('❯ Aliases', `\`${aliases.slice(1).join('` `')}\``)
+        }
+            
+        if (description.usage) {
+            embed.addField('❯ Usage', `\`${prefix + aliases[0]} ${description.usage}\``);
+        }
+            
+        if (description.examples) {
+            embed.addField('❯ Examples', description.examples.map((ex: string) => `\`${prefix + aliases[0]} ${ex}\`\n`).join(' '));
+        }
 
         return message.util?.send(embed);
     }
