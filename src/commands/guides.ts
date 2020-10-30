2d7ca5ffa6a238c098cccce1c81fd42aa6b578a5
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2020-10-29 22:33:17 -0400

    style: fix line breaks

--- src/commands/guides.ts
+++ src/commands/guides.ts
@@ -4,7 +4,7 @@ import { stripIndents } from 'common-tags';
 import { cmd } from '../utils/Constants';
 
 export default class Guides extends Command {
-    constructor() {
+    public constructor() {
         super('guides', {
             aliases: ['guides'],
             description: {
@@ -13,25 +13,17 @@ export default class Guides extends Command {
         });
     }
 
-    async exec(message: Message) {
+    public async exec(message: Message) {
         const embed = new MessageEmbed()
             .setTitle('Guides & Resources')
-            .addField(
-                '❯ Guides',
-                stripIndents`
-            • [General](https://bread.gitbook.io/guides/general)
-            • [Mechanics](https://bread.gitbook.io/guides/mechanics)
-            • [Gameplay](https://bread.gitbook.io/guides/gameplay)
-            • [Events](https://bread.gitbook.io/guides/events)`,
-                true
-            )
-            .addField(
-                '❯ Resources',
-                stripIndents`
-            • [Guide Collection](https://bread.gitbook.io/guides/)
-            • [r/Whitesushii](https://www.reddit.com/r/whitesushii/)`,
-                true
-            )
+            .addField('❯ Guides', stripIndents`
+                • [General](https://bread.gitbook.io/guides/general)
+                • [Mechanics](https://bread.gitbook.io/guides/mechanics)
+                • [Gameplay](https://bread.gitbook.io/guides/gameplay)
+                • [Events](https://bread.gitbook.io/guides/events)`, true)
+            .addField('❯ Resources', stripIndents`
+                • [Guide Collection](https://bread.gitbook.io/guides/)
+                • [r/Whitesushii](https://www.reddit.com/r/whitesushii/)`, true)
             .setColor('249EA0');
 
         return message.util?.send(embed);
