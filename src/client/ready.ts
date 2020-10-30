8cb3636c760c3d63e912bb49a7d03ed9448848e7
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2020-10-29 22:17:05 -0400

    refactor: move ready listener

--- src/client/ready.ts
+++ src/client/ready.ts
@@ -1,14 +1,14 @@
 import { Listener } from 'discord-akairo';
 
 export default class Ready extends Listener {
-    constructor() {
+    public constructor() {
         super('ready', {
             emitter: 'client',
             event: 'ready'
         });
     }
 
-    exec() {
+    public exec() {
         console.log(this.client.user.tag);
     }
 }
