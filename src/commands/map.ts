eb4623736585c993ae1ec4a4261f43bc5abff144
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2020-12-08 17:25:39 -0500

    feat(map): add support for Dismal Maze

--- src/commands/map.ts
+++ src/commands/map.ts
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 import { Command } from 'discord-akairo';
-import moment, { Moment } from 'moment-timezone';
+import { Temporal } from 'proposal-temporal'
 import { MessageEmbed, Message } from 'discord.js';
 import { Listing, Post } from '../utils/interfaces';
-import { cmd, dateRegex, months } from '../utils/Constants';
+import { cmd, LONG_DATE_FORMAT } from '../utils/Constants';
 
 export default class Map extends Command {
     public constructor() {
@@ -13,71 +13,94 @@ export default class Map extends Command {
                 content: cmd.map.description,
                 options: [
                     {
-                        flags: '[-d=, --date=]',
-                        description: cmd.map.options
+                        flag: '-d=, --date=',
+                        description: cmd.map.options.date
+                    },
+                    {
+                        flag: '--dismal',
+                        description: cmd.map.options.mode
                     }
                 ],
-                usage: '[-d=, --date=]',
-                examples: ['map', '--date=2020-01-27', '-d=2019-09-05']
+                usage: '[--dismal] [-d=, --date=]',
+                examples: [
+                    '--dismal', 
+                    '--dismal --date=2020-12-04', 
+                    '--date=2020-01-27', 
+                    '-d=2019-09-05'
+                ]
             },
             args: [
                 {
                     id: 'date',
-                    type: (_, phrase: string) => {
-                        const match = phrase.match(dateRegex)?.[0]
-                        const date = moment(match)
-
-                        if (!match || !date.isValid()) return null
-                        return date
-                    },
+                    type: 'string',
                     match: 'option',
                     flag: ['--date=', '-d=']
+                },
+                {
+                    id: 'dismal',
+                    type: 'string',
+                    match: 'flag',
+                    flag: '--dismal'
                 }
             ]
         });
     }
 
-    public async exec(message: Message, { date }: { date: Moment }) {
-        const formatted = date?.format('YYYY-MM-DD')
+    public async exec(message: Message, { date, dismal }: { date: any, dismal: boolean }) {
+        const mode = dismal ? 'Dismal Maze' : 'Arcane Labyrinth'
+        date ? date = Temporal.PlainDate.from(date) : null
 
         if (!date) {
-            try {
-                const { data }: Listing<Post> = await (await fetch(`${process.env.REDDIT_URL}/new.json?limit=1`)).json()
+            const { data }: Listing<Post> = await this.fetch(`${process.env.REDDIT_URL}/new.json?limit=2`)
 
-                return message.util?.send(this.embed(data.children[0]));
-            } catch (err) {
-                return message.util?.send(cmd.map.err_fetch);
+            if (data) {
+                const [map] = data.children.filter(({ data }) => {
+                    return data.title.match(dismal ? /Dismal Maze/i : /Lab Path/i)
+                })
+
+                return message.util?.send(this.embed(map, mode));
             }
+
+            return message.util?.send(cmd.map.err_fetch);
         }
 
-        const files: File[] = await (await fetch(`${process.env.GITHUB_URL}/${date.year()}/${months[date.month()]}`)).json()
-        const file = files?.filter((file) => file.name.includes(formatted))[0]
+        const files: File[] = await this.fetch(`${process.env.GITHUB_URL}/${mode.split(' ').join('')}/${this.url(date)}`)
+        const [file] = files?.filter((file) => file.name.includes(date))
 
         if (!files || !file) {
-            return message.util?.send(cmd.map.err_map(formatted));
+            return message.util?.send(cmd.map.err_map(date));
         }
 
-        const id = file.name.match(/(?<=#).+(?=\.png)/)[0]
-        const [{ data }]: Listing<Post>[] = await (await fetch(`${process.env.REDDIT_URL}/comments/${id}.json`)).json()
+        const [id] = file.name.match(/(?<=#).+(?=\.png)/)
+        const [{ data }]: Listing<Post>[] = await this.fetch(`${process.env.REDDIT_URL}/comments/${id}.json`)
 
         if (data) {
-            return message.util?.send(this.embed(data.children[0]))
+            return message.util?.send(this.embed(data.children[0], mode))
         }
 
         return message.util?.send(cmd.map.err_map(id));
     }
 
-    private embed({ data }: Post): MessageEmbed {
-        const time = moment.tz(data.created_utc * 1000, 'America/New_York');
+    private embed({ data }: Post, mode: string): MessageEmbed {
+        const nano = Temporal.Instant.fromEpochSeconds(data.created_utc).epochNanoseconds
+        const time = new Temporal.ZonedDateTime(nano, 'America/New_York')
 
-        const embed = new MessageEmbed()
-            .setTitle(time.add(1, 'd').format('MMMM DD, YYYY'))
+        const formatted = time.add({ days: 1 }).toLocaleString('en-US', LONG_DATE_FORMAT)
+
+        return new MessageEmbed()
+            .setTitle(`${mode} – ${formatted}`)
             .setURL(`https://reddit.com/${data.permalink}`)
             .setImage(data.url)
             .setFooter(`Posted by u/${data.author}`)
-            .setTimestamp(data.created_utc * 1000)
+            .setTimestamp(time.toInstant().epochMilliseconds)
             .setColor(0xFF5700);
+    }
 
-        return embed
+    private async fetch(url: string): Promise<any> {
+        return (await fetch(url)).json()
+    }
+
+    private url(date: Temporal.PlainDate): string {
+        return `${date.year}/${date.month}_${date.toLocaleString('en-US', { month: 'long' })}`
     }
 }
